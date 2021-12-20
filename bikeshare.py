9b3c82821ad570e385b4d405902866ebd88509a9
ralphschuhwerk <96065876+ralphschuhwerk@users.noreply.github.com>
2021-12-20 11:02:39 +0100

    Deleting the month filter

--- bikeshare.py
+++ bikeshare.py
@@ -6,6 +6,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+"""Simulate some refactoring part one"""
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
